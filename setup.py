890fa1e4a60bb051042484365de358e245ebc51e
Dimpal Goyal <dimpalgoyal129@gmail.com>
2025-11-07 06:25:35 +0000

    chore: add minimal setup and requirements files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name="todolist-cli",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="A simple Python CLI To-Do application",
+    author="Dimpal Goyal",
+)
